a02406abe49890fbf03d0d105e4997e8d2ec3ffc
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-09 14:12:15 -0300

    feat(mpris): add D-Bus constants for MPRIS interfaces and paths

---
+++ internal/mpris/constants.go
@@ -0,0 +1,14 @@
+package mpris
+
+const (
+	MPRISNamespace      = "org.mpris.MediaPlayer2"
+	MPRISObjectPath     = "/org/mpris/MediaPlayer2"
+	MPRISInterface      = "org.mpris.MediaPlayer2"
+	PlayerInterface     = "org.mpris.MediaPlayer2.Player"
+	PropertiesInterface = "org.freedesktop.DBus.Properties"
+
+	DBusObjectPath       = "/org/freedesktop/DBus"
+	DbusDestination      = "org.freedesktop.DBus"
+	DbusListNames        = "org.freedesktop.DBus.ListNames"
+	DbusPropertiesGetAll = "org.freedesktop.DBus.Properties.GetAll"
+)
